0a4094af366d4b2effd06d1cdf2b3bb41f67aa12
Richard Torruellas <richard.torruellas@am.sony.com>
2016-05-25 17:34:43 -0700

    removing stuffs

--- app/mixins/flux-component.js
+++ app/mixins/flux-component.js
@@ -7,19 +7,9 @@ export default Ember.Mixin.create({
         if (!this.store) {
             throw new Error('You must have a stores array property');
         }
-        if (this.willRender() !== 'flux-component:willRender') {
-            throw new Error('overwriting willRender while extemdomg flux-component will cause issues');
-        }
         this.data = this.getStateFromStores();
         this.dispatcher = Welp.WelpDispatcher;
         this.store.addChangeListener(this.handleStoresChanged.bind(this));
-    },
-    willRender(...args) {
-        this._super(args);
-        return 'flux-component:willRender';
-    },
-    didReceiveAttrs(...args) {
-        console.log(args);
     },
     getStateFromStores() {
         return this.store.getDataStructure().toJS();
